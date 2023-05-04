9b61ba8fed309bbd7854677d10df42c1f3333225
ProdipKumarShil <prodipkrishna01@gmail.com>
2023-05-04 19:48:28 +0600

    sent chefs json

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const chefsJSON = require('./data/chefs.json')
+
 app.get('/', (req, res) => {
   res.send('recipe is cooking')
 })
 
-
+app.get('/chefs', (req, res) => {
+  res.send(chefsJSON)
+})
 
 app.listen(port, () => {
   console.log('recipe is cooking in ', port)
